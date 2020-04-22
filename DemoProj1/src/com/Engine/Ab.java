5bc7a2c00079d6e20722701545f352a7b7e950c0
milind <milind@DESKTOP-0FEKVC8>
2020-04-22 15:09:11 +0530

    changes to two packages engine and reports with ab.java and cd.java

---
+++ DemoProj1/src/com/Engine/Ab.java
@@ -0,0 +1,10 @@
+package com.Engine;
+
+public class Ab 
+{
+	public static void m1()
+	{
+		System.out.println("this is tls m1 method");
+	}
+
+}
---
+++ DemoProj1/src/com/reports/Cd.java
@@ -0,0 +1,11 @@
+package com.reports;
+
+public class Cd 
+
+{
+	public static void m2()
+	{
+		System.out.println("this is tls method 2 ");
+	}
+
+}
