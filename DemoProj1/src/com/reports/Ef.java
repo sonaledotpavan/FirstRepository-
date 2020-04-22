8b8095e92d8411b939d4e1d1076cd192130943a5
milind <milind@DESKTOP-0FEKVC8>
2020-04-22 18:15:50 +0530

    again solving conflict by dev2 code priority

--- DemoProj1/src/com/reports/Ef.java
+++ DemoProj1/src/com/reports/Ef.java
@@ -4,11 +4,9 @@ public class Ef
 {
 	public static void m3()
 	{
-<<<<<<< HEAD
+
 		System.out.println("hello 1234");
-=======
-		System.out.println("hello: ABCD");
->>>>>>> branch 'master' of https://github.com/sonaledotpavan/FirstRepository-.git
+
 	}
 	
 	public static void main(String[] args) 
