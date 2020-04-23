20646fe5aec16a6e7fe3e51f9cdb63dbb1bc2d20
Pavan <sonale.pavan@gmail.com>
2020-04-23 23:44:43 +0530

    Dev1: completed m4()

--- DemoProj1/src/com/reports/Ef.java
+++ DemoProj1/src/com/reports/Ef.java
@@ -10,11 +10,23 @@ public class Ef
 	{
 		System.out.println("This is m1 method");
 	}
+
 	public static void m5()
 	{
 		System.out.println("m5 method");
 	}
 	
+
+	public static void m4()
+	{
+		//only 30% coding of m4() is completed 
+		//but TL wants to merge branches.. but dev1's work of m4 not completed 100%
+		//So, save it temporary and later dev1 can reuse m4 method
+		//so, go for 'stagging'
+		//--------------------------------------------
+		//now its completed
+	}
+
 	public static void main(String[] args) 
 	{
 		Ef e=new Ef();
