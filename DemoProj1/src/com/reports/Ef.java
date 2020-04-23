2fd6dd908b122fdecc59adf1751a6f6444f21aa0
Pavan <sonale.pavan@gmail.com>
2020-04-23 23:25:55 +0530

    Dev1:Branch1:m5 method

--- DemoProj1/src/com/reports/Ef.java
+++ DemoProj1/src/com/reports/Ef.java
@@ -10,6 +10,10 @@ public class Ef
 	{
 		System.out.println("This is m1 method");
 	}
+	public static void m5()
+	{
+		System.out.println("m5 method");
+	}
 	
 	public static void main(String[] args) 
 	{
